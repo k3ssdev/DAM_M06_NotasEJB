72ad5056d3b37cbc5f906b0e8d1a051fb9ac57f0
k3ssdev <albertoperezdelrio@gmail.com>
2023-05-05 00:02:34 +0200

    update

--- build/generated-sources/ap-source-output/clasesPOJO/Notas_.java
+++ build/generated-sources/ap-source-output/clasesPOJO/Notas_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-04T13:37:16")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-05-04T16:09:05")
 @StaticMetamodel(Notas.class)
 public class Notas_ { 
 
